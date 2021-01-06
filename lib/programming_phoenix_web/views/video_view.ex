1f5952a615d985ddded43cab133b1d425722bca4
recroot89 <recroot89@gmail.com>
2021-01-06 18:40:18 +0500

    add model constraints

--- lib/programming_phoenix_web/views/video_view.ex
+++ lib/programming_phoenix_web/views/video_view.ex
@@ -1,3 +1,7 @@
 defmodule ProgrammingPhoenixWeb.VideoView do
   use ProgrammingPhoenixWeb, :view
+
+  def category_select_options(categories) do
+    for category <- categories, do: {category.name, category.id}
+  end
 end
